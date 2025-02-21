4e03b9fd2f98c6aa64465c5a0abeeda0e37b0151
udaykumar2507 <uk25072004@gmail.com>
2025-02-21 08:20:48 +0530

    Day 10

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,35 +1,40 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+// import './App.css'
+import Home from "./components/FunctionalComponent/Home.jsx";
+import About from "./components/FunctionalComponent/About.jsx";
+import Contact from "./components/FunctionalComponent/Contact.jsx";
+import Gallery from "./components/FunctionalComponent/Gallery.jsx";
+import Signup from "./components/FunctionalComponent/Signup.jsx";
+import Navbar from "./components/FunctionalComponent/Navbar.jsx";
+import UseEffect from "./components/FunctionalComponent/Hooks/UseEffects.jsx";
+import UseState from "./components/FunctionalComponent/Hooks/UseState.jsx";
+import UseEffectAPI from "./components/FunctionalComponent/Hooks/UseEffectAPI.jsx";
+import UseRef from "./components/FunctionalComponent/Hooks/UseRef.jsx";
+import UseMemo from "./components/FunctionalComponent/Hooks/UseMemo.jsx";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div>
+      <main>
+        <BrowserRouter>
+          <Navbar />{" "}
+          {/* navbar should be inside the browser router tag because as the navbar uses the Link from BrowserRouter*/}
+          <Routes>
+            <Route path="/" element={<Home sjit="st.joseph" />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/contact" element={<Contact />} />
+            <Route path="/use-effect" element={<UseEffect />} />
+            <Route path="/use-state" element={<UseState />} />
+            <Route path="/use-effect-api" element={<UseEffectAPI />} />
+            <Route path="/use-ref" element={<UseRef />} />
+            <Route path="/use-memo" element={<UseMemo />} />
+            <Route path="/gallery" element={<Gallery />} />
+            <Route path="/signup" element={<Signup />} />
+          </Routes>
+        </BrowserRouter>
+      </main>
+    </div>
+  );
 }
 
-export default App
+export default App;
